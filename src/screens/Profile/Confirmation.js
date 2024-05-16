9b8d55bf66fa4f2a624437d963336cdd23d247a4
enkhturBaina <eegii.111.gg@gmail.com>
2024-05-16 16:44:37 +0800

    conbfirmation doing

--- src/screens/Profile/Confirmation.js
+++ src/screens/Profile/Confirmation.js
@@ -1,112 +1,354 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  Linking,
-  ScrollView,
-  StatusBar,
-  Platform,
-} from "react-native";
-import React, { useContext, useState } from "react";
-import { Icon } from "@rneui/base";
+import { StyleSheet, View, Platform, ScrollView, KeyboardAvoidingView, Text, TouchableOpacity } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { GRAY_ICON_COLOR, MAIN_COLOR_GRAY, SERVER_URL, X_API_KEY } from "../../constant";
 import MainContext from "../../contexts/MainContext";
+import LoanInput from "../../components/LoanInput";
+import CustomDialog from "../../components/CustomDialog";
+import axios from "axios";
 import CustomSnackbar from "../../components/CustomSnackbar";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
-import bg from "../../../assets/splash_bg.png";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import Constants from "expo-constants";
-import { GRAY_ICON_COLOR, MAIN_COLOR_GRAY } from "../../constant";
-import { Divider } from "react-native-paper";
+import GradientButton from "../../components/GradientButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import EditProfileSkeleton from "../../components/Skeletons/EditProfileSkeleton";
+import { Icon } from "@rneui/base";
+import UserTabData from "../../refs/UserTabData";
+import BottomSheet from "../../components/BottomSheet";
 
 const Confirmation = (props) => {
-  const state = useContext(MainContext);
+	const state = useContext(MainContext);
 
-  const tabBarHeight = useBottomTabBarHeight();
-  const onToggleSwitch = () => {
-    onToggleSnackBar("Ирц бүртгэл сануулах тохиргоо хийгдлээ");
-  };
+	const tabBarHeight = useBottomTabBarHeight();
 
-  const [visibleDialog, setVisibleDialog] = useState(false); //Dialog харуулах
-  const [dialogType, setDialogType] = useState("warning"); //Dialog харуулах төрөл
-  const [dialogText, setDialogText] = useState("Апп -с гарах уу?"); //Dialog -н текст
+	const [visibleDialog, setVisibleDialog] = useState(false); //Dialog харуулах
+	const [dialogType, setDialogType] = useState("warning"); //Dialog харуулах төрөл
+	const [dialogText, setDialogText] = useState(""); //Dialog -н текст
 
-  const [visibleSnack, setVisibleSnack] = useState(false);
-  const [snackBarMsg, setSnackBarMsg] = useState("");
+	const [loadingProfileData, setLoadingProfileData] = useState(false);
+	const [profileData, setProfileData] = useState(null);
 
-  //Snacbkbar харуулах
-  const onToggleSnackBar = (msg) => {
-    setVisibleSnack(!visibleSnack);
-    setSnackBarMsg(msg);
-  };
+	const [visibleSnack, setVisibleSnack] = useState(false);
+	const [snackBarMsg, setSnackBarMsg] = useState("");
 
-  //Snacbkbar хаах
-  const onDismissSnackBar = () => setVisibleSnack(false);
+	const [data, setData] = useState(""); //BottomSheet рүү дамжуулах Дата
+	const [uselessParam, setUselessParam] = useState(false); //BottomSheet -г дуудаж байгааг мэдэх гэж ашиглаж байгамоо
+	const [fieldName, setFieldName] = useState(""); //Context -н аль утгыг OBJECT -с update хийхийг хадгалах
+	const [displayName, setDisplayName] = useState(""); //LOOKUP -д харагдах утга (display value)
 
-  return (
-    <SafeAreaProvider
-      style={{
-        flex: 1,
-        paddingTop: Constants.statusBarHeight,
-        backgroundColor: "#fff",
-        paddingBottom: tabBarHeight,
-      }}
-    >
-      <ScrollView
-        contentContainerStyle={{ flexGrow: 1 }}
-        showsVerticalScrollIndicator={false}
-        bounces={false}
-      >
-        <StatusBar
-          translucent
-          barStyle={Platform.OS == "ios" ? "dark-content" : "default"}
-        />
-        <Text>Confirmation</Text>
-      </ScrollView>
-    </SafeAreaProvider>
-  );
+	const setLookupData = (data, field, display) => {
+		// console.log("refRBSheet", refRBSheet);
+		setData(data); //Lookup -д харагдах дата
+		setFieldName(field); //Context -н object -н update хийх key
+		setDisplayName(display); //Lookup -д харагдах датаны текст талбар
+		setUselessParam(!uselessParam);
+	};
+
+	//Snacbkbar харуулах
+	const onToggleSnackBar = (msg) => {
+		setVisibleSnack(!visibleSnack);
+		setSnackBarMsg(msg);
+	};
+
+	//Snacbkbar хаах
+	const onDismissSnackBar = () => setVisibleSnack(false);
+	const getProfileData = async () => {
+		setLoadingProfileData(true);
+		await axios
+			.get(`${SERVER_URL}authentication`, {
+				headers: {
+					"X-API-KEY": X_API_KEY,
+					Authorization: `Bearer ${state.token}`
+				}
+			})
+			.then((response) => {
+				// console.log("AAA", response.data.response?.user);
+				setProfileData(response.data.response?.user);
+			})
+			.catch((error) => {
+				console.error("Error fetching EditProfile=>get ProfileData=>:", error);
+				if (error.response.status == "401") {
+					state.setIsLoggedIn(false);
+					state.setErrorMsg("Токены хүчинтэй хугацаа дууссан байна. Дахин нэвтэрнэ үү");
+				}
+			})
+			.finally(() => {
+				setLoadingProfileData(false);
+			});
+	};
+
+	useEffect(() => {
+		// getProfileData();
+	}, []);
+
+	const saveProfileData = async () => {
+		if (!profileData.lastName) {
+			onToggleSnackBar("Овог оруулна уу.");
+		} else if (!profileData.firstName) {
+			onToggleSnackBar("Нэр оруулна уу.");
+		} else if (!profileData.jobPosition) {
+			onToggleSnackBar("Албан тушаал оруулна уу.");
+		} else if (!profileData.phone) {
+			onToggleSnackBar("Утасны дугаар оруулна уу.");
+		} else if (!profileData.address) {
+			onToggleSnackBar("Хаяг оруулна уу.");
+		} else {
+			axios
+				.patch(
+					`${SERVER_URL}users/${state.userId}`,
+					{
+						lastName: profileData?.LastName,
+						firstName: profileData?.firstName,
+						jobPosition: profileData?.jobPosition,
+						phone: profileData?.phone,
+						address: profileData?.address
+					},
+					{
+						headers: {
+							"x-api-key": `${X_API_KEY}`,
+							Authorization: `Bearer ${state.token}`
+						}
+					}
+				)
+				.then(async (response) => {
+					if (response.data) {
+						// console.log("response.data", response.data);
+						state.setFirstName(profileData?.firstName);
+						state.setLastName(profileData?.lastName);
+						state.setPhone(profileData?.phone);
+
+						await AsyncStorage.setItem(
+							"user",
+							JSON.stringify({
+								accessToken: state?.token,
+								email: response.data?.response?.email,
+								id: response.data?.response?.id,
+								firstName: response.data?.response?.firstName,
+								lastName: response.data?.response?.lastName,
+								phone: response.data?.response?.phone
+							})
+						);
+						state.setUserData((prevState) => ({
+							...prevState,
+							email: response.data?.response?.email,
+							firstName: response.data?.response?.firstName,
+							lastName: response.data?.response?.lastName,
+							phone: response.data?.response?.phone
+						}));
+						setDialogText("Амжилттай.");
+						setVisibleDialog(true);
+					}
+				})
+				.catch(function (error) {
+					if (error.response.status == "401") {
+						state.setIsLoggedIn(false);
+						state.setErrorMsg("Токены хүчинтэй хугацаа дууссан байна. Дахин нэвтэрнэ үү");
+					}
+				});
+		}
+	};
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: "#fff",
+				paddingBottom: tabBarHeight
+			}}
+		>
+			<CustomSnackbar visible={visibleSnack} dismiss={onDismissSnackBar} text={snackBarMsg} topPos={1} />
+			<KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={{ flex: 1 }}>
+				{loadingProfileData ? (
+					<EditProfileSkeleton />
+				) : (
+					<View style={{ flex: 1 }}>
+						<ScrollView contentContainerStyle={styles.scrollContainer} bounces={false}>
+							<View style={styles.touchableSelectContainer}>
+								<Text style={styles.label}>Хэрэглэгчийн төрөл</Text>
+								<TouchableOpacity
+									style={styles.touchableSelect}
+									onPress={() => {
+										setLookupData(UserTabData, "userType", "title");
+									}}
+								>
+									<Text style={styles.selectedText} numberOfLines={1}>
+										{state?.serviceData.userType != "" ? state?.serviceData.userType?.title : "Сонгох"}
+									</Text>
+									<Icon name="keyboard-arrow-down" type="material-icons" size={30} color={GRAY_ICON_COLOR} />
+								</TouchableOpacity>
+							</View>
+							<View style={styles.touchableSelectContainer}>
+								<Text style={styles.label}>Үйл ажиллагааны үндсэн чиглэл</Text>
+								<TouchableOpacity
+									style={styles.touchableSelect}
+									onPress={() => {
+										setLookupData(UserTabData, "userType", "title");
+									}}
+								>
+									<Text style={styles.selectedText} numberOfLines={1}>
+										{state?.serviceData.userType != "" ? state?.serviceData.userType?.title : "Сонгох"}
+									</Text>
+									<Icon name="keyboard-arrow-down" type="material-icons" size={30} color={GRAY_ICON_COLOR} />
+								</TouchableOpacity>
+							</View>
+							<Text style={styles.label}>Зураг оруулах</Text>
+							<View style={styles.gridContainer}>
+								<TouchableOpacity onPress={() => {}} style={styles.gridItem}>
+									<Text style={styles.featureText}>Үнэмлэхний урд талын зураг</Text>
+									<Icon name="image" type="feather" size={20} color={GRAY_ICON_COLOR} />
+								</TouchableOpacity>
+								<TouchableOpacity onPress={() => {}} style={styles.gridItem}>
+									<Text style={styles.featureText}>Селфи зураг</Text>
+									<Icon name="image" type="feather" size={20} color={GRAY_ICON_COLOR} />
+								</TouchableOpacity>
+								<TouchableOpacity onPress={() => {}} style={styles.gridItem}>
+									<Text style={styles.featureText}>Үнэмлэхний ард талын зураг</Text>
+									<Icon name="image" type="feather" size={20} color={GRAY_ICON_COLOR} />
+								</TouchableOpacity>
+								<TouchableOpacity onPress={() => {}} style={styles.gridItem}>
+									<Text style={styles.featureText}>Лого</Text>
+									<Icon name="image" type="feather" size={20} color={GRAY_ICON_COLOR} />
+								</TouchableOpacity>
+							</View>
+							<LoanInput
+								label="Байгууллагын нэр"
+								value={profileData?.lastName}
+								onChangeText={(e) =>
+									setProfileData((prevState) => ({
+										...prevState,
+										lastName: e
+									}))
+								}
+							/>
+							<LoanInput
+								label="Байгууллагын регистрийн дугаар"
+								value={profileData?.firstName}
+								onChangeText={(e) =>
+									setProfileData((prevState) => ({
+										...prevState,
+										firstName: e
+									}))
+								}
+							/>
+							<LoanInput
+								label="Веб хуудас"
+								value={profileData?.jobPosition}
+								onChangeText={(e) =>
+									setProfileData((prevState) => ({
+										...prevState,
+										jobPosition: e
+									}))
+								}
+							/>
+							<LoanInput
+								label="Байгууллагын үйл ажилгааний чиглэл"
+								value={profileData?.phone}
+								keyboardType="number-pad"
+								maxLength={8}
+								onChangeText={(e) =>
+									setProfileData((prevState) => ({
+										...prevState,
+										phone: e
+									}))
+								}
+							/>
+							<LoanInput
+								label="Байгууллагын танилцуулга ба ажлын туршлага"
+								value={profileData?.address}
+								onChangeText={(e) =>
+									setProfileData((prevState) => ({
+										...prevState,
+										address: e
+									}))
+								}
+								multiline={true}
+								textAlignVertical="top"
+							/>
+							<View className="w-full mt-2">
+								<GradientButton text="Хадгалах" action={saveProfileData} />
+							</View>
+						</ScrollView>
+					</View>
+				)}
+			</KeyboardAvoidingView>
+			<CustomDialog
+				visible={visibleDialog}
+				confirmFunction={() => {
+					setVisibleDialog(false);
+					// dialogType == "success" && props.navigation.goBack();
+				}}
+				declineFunction={() => {}}
+				text={dialogText}
+				confirmBtnText="Хаах"
+				DeclineBtnText=""
+				type={dialogType}
+			/>
+			<BottomSheet
+				bodyText={data}
+				dragDown={true}
+				backClick={true}
+				type="lookup"
+				fieldName={fieldName}
+				displayName={displayName}
+				lookUpType="profile"
+				handle={uselessParam}
+				action={(e) => {
+					state?.setServiceData((prevState) => ({
+						...prevState,
+						[fieldName]: e
+					}));
+				}}
+			/>
+		</View>
+	);
 };
 
 export default Confirmation;
 
 const styles = StyleSheet.create({
-  headerBg: {
-    width: "100%",
-    height: 150,
-    resizeMode: "cover",
-  },
-  userIcon: {
-    width: 100,
-    height: 100,
-    resizeMode: "contain",
-    borderWidth: 4,
-    borderRadius: 120,
-    borderColor: "#fff",
-  },
-  gridMenus: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 25,
-    marginBottom: 20,
-  },
-  lastText: {
-    color: "red",
-    fontWeight: "500",
-    marginLeft: 20,
-  },
-  menuText: {
-    color: GRAY_ICON_COLOR,
-    fontWeight: "500",
-    marginLeft: 20,
-  },
-  profileCircle: {
-    position: "absolute",
-    flexDirection: "row",
-    top: 100,
-    paddingHorizontal: 20,
-    paddingBottom: 30,
-    borderBottomWidth: 1,
-    borderBottomColor: MAIN_COLOR_GRAY,
-  },
+	scrollContainer: {
+		flexGrow: 1,
+		backgroundColor: "#fff",
+		paddingHorizontal: 20,
+		paddingTop: 10
+	},
+	touchableSelectContainer: {
+		marginBottom: 10
+	},
+	touchableSelect: {
+		flexDirection: "row",
+		width: "100%",
+		justifyContent: "space-between",
+		borderRadius: 12,
+		backgroundColor: MAIN_COLOR_GRAY,
+		height: 48,
+		alignItems: "center",
+		paddingLeft: 15,
+		paddingRight: 10
+	},
+	label: {
+		fontWeight: "bold",
+		padding: 5
+	},
+	selectedText: {
+		fontWeight: "500",
+		color: GRAY_ICON_COLOR,
+		width: "90%"
+	},
+	gridContainer: {
+		flexDirection: "column"
+	},
+	gridItem: {
+		marginBottom: 10,
+		borderRadius: 4,
+		height: 40,
+		flexDirection: "row",
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: MAIN_COLOR_GRAY,
+		width: "100%" // is 50% of container width
+	},
+	featureText: {
+		color: "#798585",
+		marginRight: 5,
+		fontSize: 12
+	}
 });
